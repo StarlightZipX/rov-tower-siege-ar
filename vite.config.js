4e40287b4d7333642be83fb972d93caca710b755
RoV-AR <rov-ar@project.local>
2026-08-17 11:01:46 +0700

    Setup GitHub Pages Action

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/rov-tower-siege-ar/',
   plugins: [basicSsl()],
   server: {
     host: true
